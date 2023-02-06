0c4c89b3d0ea760969ca57b861aa7db5f3df6b66
Dan Hagen <dan.hagen@centralia.edu>
2023-02-06 13:17:14 -0800

    Add project files.

---
+++ IT330GitHubStarter/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IT330GitHubStarter
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = "Hello GitHub World!!";
+        }
+    }
+}
